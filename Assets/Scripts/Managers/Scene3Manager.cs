8146babf5ce0f441ec288264f9565b426e31bf95
Rob Borghese <Kabutak@discovrlabs.com>
2016-09-07 01:30:38 -0400

    Added nurse VO for Scene 3, Fixed models and menus

--- Assets/Scripts/Managers/Scene3Manager.cs
+++ Assets/Scripts/Managers/Scene3Manager.cs
@@ -5,6 +5,7 @@ public class Scene3Manager : SceneManager
 {
 	public GameObject Scene3a;
 	public GameObject Scene3b;
+	public NurseAudio NurseVO;
 	[Header("Scene 3A")]
 	public GameObject SurgIcon;
 	public VOController SurgVO;
@@ -35,7 +36,7 @@ public class Scene3Manager : SceneManager
 
 	public override void ActivateScene()
 	{
-		Invoke("ChangeMonitor", 3.0f);
+		Invoke("ChangeMonitor", 5.0f);
 	}
 
 	public override void EndVO(VOController controller)
@@ -54,13 +55,16 @@ public class Scene3Manager : SceneManager
 	{
 		if(bShouldDie)
 		{
-			Manager.SetWinState(false);
+			Scene3b.SetActive(false);
+			float delay = NurseVO.PlayClip(3);
+			Invoke("TriggerFailure", delay);
 		}
 		else
 		{
 			CorrectActions++;
 			if (CorrectActions >= 7)
 			{
+				Scene3b.SetActive(false);
 				Manager.SetWinState(true);
 			}
 		}
@@ -71,10 +75,25 @@ public class Scene3Manager : SceneManager
 		//Change monitor state and tone
 		Scene3a.SetActive(true);
 		SurgIcon.SetActive(true);
+		NurseVO.PlayClip(0);
 		for (int i = 0; i < MenusA.Length; i++)
 		{
 			MenusA[i].SetHoverable(true);
 		}
+		Invoke("TriggerEmergency", 45.0f);
+	}
+
+	void TriggerEmergency()
+	{
+		//Trigger scene 3.1
+		Scene3a.SetActive(false);
+		Scene3b.SetActive(true);
+		NurseVO.PlayClip(1);
+	}
+
+	void TriggerFailure()
+	{
+		Manager.SetWinState(false);
 	}
 
 	public void ClickSurgeon(bool Button)
---
+++ Assets/Scripts/NurseAudio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class NurseAudio : MonoBehaviour
+{
+	public AudioClip[] AudioClips;
+
+	public float PlayClip(int Index)
+	{
+		AudioSource Source = GetComponent<AudioSource>();
+		Source.clip = AudioClips[Index];
+		Source.Play();
+
+		return AudioClips[Index].length;
+	}
+}
